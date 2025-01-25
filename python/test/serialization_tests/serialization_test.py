3c19464dab049bbefbdbcf4fb4d49d7555d90f44
dschwartznyc <daniel.schwartz@ftadvisory.co>
2025-01-24 23:24:31 -0500

    interim update for serialization_test

--- python/test/serialization_tests/serialization_test.py
+++ python/test/serialization_tests/serialization_test.py
@@ -6,10 +6,11 @@ import importlib
 import glob
 import argparse
 import pytest
+
 from rune.runtime.base_data_class import BaseDataClass
 from test_helper.test_helper.dict_comp import dict_comp
 
-JSON_DIR = '/Users/dls/projects/rune/rune-common/serialization/src/test/resources/rune-serializer-round-trip-test/metakey'
+JSON_DIR = '/Users/dls/projects/rune/rune-common/serialization/src/test/resources/rune-serializer-round-trip-test/enumtypes/'
 
 def deserialize (rune_string: str) -> BaseDataClass:
     '''generate a rune object from a string'''
@@ -29,20 +30,23 @@ def deserialize_from_dict(rune_type: str, rune_dict: dict) -> BaseDataClass:
     :return: An instance of the specified class.
     """
     # Import the module and get the class
-    print('deserialize_from_dict ... getting cls ... obj_class_path:' + rune_type)
     rune_class_name = rune_type.rsplit('.')[-1]
-    print('deserialize_from_dict ... rune_class_name:' + rune_class_name)
     rune_module = importlib.import_module(rune_type)
-    print('deserialize_from_dict ... called importlib 1 ... rune_module: ' + str(rune_module))
+    print ('.... rune_type:' + rune_type)
     rune_cls = getattr(rune_module, rune_class_name)
-    print('deserialize_from_dict ... called gettar ... rune_cls :' + str(rune_cls))
 
     # Ensure the class is a subclass of BaseDataClass (from the rune runtime)
     if not issubclass(rune_cls, BaseDataClass):
         raise TypeError(f"{rune_class_name} is not a subclass of BaseDataClass")
 
+#    rune_dict_str = json.dumps(rune_dict);
+#    rune_obj = rune_cls.model_validate_json(rune_dict_str)
     rune_obj = rune_cls.model_validate (rune_dict)
-    print('deserialize_from_dict ... done')
+    print ('.... model_validate done')
+    rune_obj.resolve_references()
+    print ('.... resolve_references done')
+    rune_obj.validate_model()
+    print ('.... validate_model done')
     return rune_obj
 
 def serialize (rune_out: BaseDataClass) -> str:
@@ -62,29 +66,25 @@ def serialize (rune_out: BaseDataClass) -> str:
 json_files = glob.glob(JSON_DIR + os.sep + '**/*.json', recursive = True)
 inscope_files = []
 
-@pytest.mark.parametrize("json_file", json_files)
-def test_json_file(json_file):
-    '''Load data from the JSON file'''
-    json_file_name = json_file.split(os.sep)[-1]
-    if len(inscope_files) == 0 or json_file_name in inscope_files:
-        json_str = Path(os.path.join(JSON_DIR, json_file)).read_text(encoding='utf8')
-        try:
-            obj = deserialize(json_str)
-            print(obj)
-            json_str_out = serialize(obj)
-            dict_in = json.loads(json_str)
-            dict_out = json.loads(json_str_out)
-            assert dict_comp(dict_in, dict_out), f"failed dict comparison for {json_file_name}"
-        except Exception as error_msg:
-            print(error_msg)
-            assert False
-    else:
-        assert False, f"test not implemented for {json_file}"
-
-def main():
-    '''Run pytest programmatically'''
-    pytest_args = ['-v', __file__]  # '-v' for verbose output, '__file__' to specify the current file
-    pytest.main(pytest_args)
+def process_file(file_name, only_serialize: bool = False):
+    '''process a file'''
+    print('.... processing file: ', file_name)
+    try:
+        json_str_in = Path(file_name).read_text(encoding='utf8')
+        obj = deserialize(json_str_in)
+        json_str_out = serialize(obj)
+        if (not only_serialize):
+            dict_file_in = json.loads(json_str_in)
+            dict_file_out = json.loads(json_str_out)
+            assert dict_comp(dict_file_in, dict_file_out), f"failed dict comparison for {file_name}"
+    except Exception as error_msg:
+        print(error_msg)
+    
+def process_directory(dir_name, only_serialize: bool = False):
+    '''process all files in a directory'''
+    dir_files = glob.glob(dir_name + os.sep + '**/*.json', recursive = True)
+    for dir_file in dir_files:
+        process_file(dir_file, only_serialize)
     
 def process_files ():
     '''skip tests and show deserialized and serialized data'''
@@ -101,30 +101,39 @@ def process_files ():
             except Exception as ex:
                 print('error processing file:', json_file, '\nexception\n', ex)
 
+@pytest.mark.parametrize("json_file", json_files)
+def test_json_file(json_file):
+    '''Load data from the JSON file'''
+    json_file_name = json_file.split(os.sep)[-1]
+    if len(inscope_files) == 0 or json_file_name in inscope_files:
+        process_file(os.path.join(JSON_DIR, json_file))
+    else:
+        assert False, f"test not implemented for {json_file}"
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--test', help='Run Unit Tests', action="store_true")
     parser.add_argument('-p', '--process', 
                         help='Show Deserialized and Serialized Data', 
                         action="store_true")
-    parser.add_argument('-x', '--execute', help='Execute Test On a Specific JSON')
+    parser.add_argument('-f', '--file', help='Test a JSON File')
+    parser.add_argument('-d', 
+                        '--directory', 
+                        help='Test All Files in a Directory')
     args = parser.parse_args()
     if args.test:
         print('run tests')
-        main()
+        pytest_args = ['-v', __file__]  # '-v' for verbose output, '__file__' to specify the current file
+        pytest.main(pytest_args)
     elif args.process:
         print('process files')
         process_files()
-    elif args.execute:
-        print('testing file: ', args.execute)
-        rune_str = Path(os.path.join(JSON_DIR, args.execute)).read_text(encoding='utf8')
-        print('json_str:', rune_str)
-        try:
-            rune = deserialize(rune_str)
-            print (rune)
-            rune_str_out = serialize(rune)
-            print (rune_str_out)
-        except Exception as ex:
-            print('error processing file:', args.execute, '\nexception\n', ex, '\n', rune_str)
+    elif args.directory:
+        print('testing files in the directory: ', args.directory)
+        process_directory(args.directory)
+    elif args.file:
+        print('testing file: ', args.file)
+        process_file (args.file)
+        rune_str = Path(args.file).read_text(encoding='utf8')
     else:
         parser.print_help()
