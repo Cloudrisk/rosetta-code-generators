1b97465c8c45b68541f5dde02caa509181646e08
dschwartznyc <daniel.schwartz@ftadvisory.co>
2025-01-26 11:57:45 -0500

    support for both @key/@ref and @key:external/@ref:external

--- python/test/serialization_tests/serialization_test.py
+++ python/test/serialization_tests/serialization_test.py
@@ -7,6 +7,8 @@ import argparse
 import pytest
 
 from rune.runtime.base_data_class import BaseDataClass
+from rune.runtime.object_registry import _OBJECT_REGISTRY
+
 from test_helper.test_helper.dict_comp import dict_comp
 
 JSON_DIR = '/Users/dls/projects/rune/rune-common/serialization/src/test/resources/rune-serializer-round-trip-test/'
@@ -14,22 +16,26 @@ JSON_DIR = '/Users/dls/projects/rune/rune-common/serialization/src/test/resource
 def extract_dir_and_file (path_name):
     return Path(path_name).parent.name + '/' + os.path.basename(path_name)
 
-def process_file(path_name, compare: bool = True) -> bool:
+def process_file(path_name, compare: bool = True, reset_registry: bool = True, show_results: bool = False) -> bool:
     '''process a file'''
     file_name =  extract_dir_and_file(path_name)
     try:
         json_str_in = Path(path_name).read_text(encoding='utf8')
+        if (reset_registry):
+            _OBJECT_REGISTRY = {}
         obj = BaseDataClass().rune_deserialize(json_str_in)
         json_str_out = obj.rune_serialize()
         if compare:
             dict_file_in = json.loads(json_str_in)
             dict_file_out = json.loads(json_str_out)
             result = dict_comp(dict_file_in, dict_file_out)
-            result_str = 'serialization matches' if result else 'serialization does not match'
-            print('.... processed file: ', file_name, ' result: ', result_str)
+            if (show_results):
+                result_str = 'serialization matches' if result else 'serialization does not match'
+                print('.... processed file: ', file_name, ' result: ', result_str)
             return result
     except Exception as error_msg:
-        print('.... something failed for file', file_name, ' exception:', error_msg)
+        if (show_results):
+            print('.... exception processing file:', file_name, ' exception:', error_msg)
         return False
 
 def process_directory(dir_name):
@@ -38,14 +44,22 @@ def process_directory(dir_name):
     results = []
     for path_name in path_names:
         results.append({"path_name": path_name, "result": process_file (path_name)})
-    print('---- result summary for dir:', dir_name)
+    matches = 0
+    failures = 0
+    print('---- result summary for dir:', dir_name, '----- matches')
     for result in results:
         file_name = extract_dir_and_file(result['path_name'])
         if (result['result']):
             print('file:', file_name, '...', 'serialization matches')
-        else:
+            matches += 1
+    print('---- result summary for dir:', dir_name, '----- failures')
+    for result in results:
+        file_name = extract_dir_and_file(result['path_name'])
+        if (not result['result']):
             print('file:', file_name, '...', 'something failed')
-            process_file(result['path_name'])
+            process_file(result['path_name'], show_results=True)
+            failures += 1
+    print('---- result summary for dir:', dir_name, ' matches:', matches, 'failures:', failures)
 
 @pytest.mark.parametrize("json_file", glob.glob(JSON_DIR + os.sep + '**/*.json', recursive=True))
 def test_json_file(json_file):
